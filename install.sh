277be971657ae8baa39c911e2537bac765872abd
fadmaeye <florian.demaeyer@ugent.be>
2014-05-24 19:07:03 +0200

    created functions in install.sh :panda_face:

--- install.sh
+++ install.sh
@@ -1,104 +1,153 @@
 #!/bin/bash
 
-echo "Creating symlinks..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -d ~/.vim ]
-then
-    echo "An existing .vim folder has been found, do you wish keep a backup(.vim.bak) of it? (y/n)"
-    read -n 1 answer
-    while [ "$answer" != "y" -a "$answer" != "n" ]
-    do
-        echo "Please answer with y or n."
-        read -n 1 answer
-    done
-    if [ "$answer" == "y" ]
+
+
+dirbackup()
+{
+    dirname="$1"
+    if [ -d ~/"$dirname" ]
     then
-        mv ~/.vim ~/.vim.bak
-    else
-        rm -rf ~/.vim
+        echo "An existing "$dirname" folder has been found, do you wish to keep a backup("$dirname".bak) of it? (y/n)"
+        read -ns 1 answer
+        while [ "$answer" != "y" -a "$answer" != "n" ]
+        do
+            echo "Please answer with y or n."
+            read -ns 1 answer
+        done
+        if [ "$answer" == "y" ]
+        then
+            mv ~/"$dirname" ~/"$dirname".bak
+        else
+            rm -rf ~/"$dirname"
+        fi
     fi
-fi
+} 
 
-if [ -d ~/.zsh ]
-then
-    echo "An existing .zsh folder has been found, do you wish keep a backup(.zsh.bak) of it? (y/n)"
-    read -n 1 answer
-    while [ "$answer" != "y" -a "$answer" != "n" ]
-    do
-        echo "Please answer with y or n."
-        read -n 1 answer
-    done
-    if [ "$answer" == "y" ]
+dirbackup .vim
+dirbackup .zsh
+dirbackup .Xrecources
+dirbackup .config
+
+localrc()
+{
+    filename="$1"
+    if [ -f ~/"$filename" ]
     then
-        mv ~/.zsh ~/.zsh.bak
-    else
-        rm -rf ~/.zsh
+        if [ ! -h ~/"$filename" ]
+        then
+            mv ~/"$filename"  ~/"$filename".local
+            echo "Your previous "$filename" is now available under ~/"$filename".local and can still be used to overwrite settings."
+        else
+            rm ~/"$filename"
+            touch ~/"$filename".local
+        fi
     fi
-fi
+}
 
-if [ -f ~/.Xresources ]
-then
-    echo "An existing .Xresources file has been found, do you wish keep a backup(.Xresources.bak) of it? (y/n)"
-    read -n 1 answer
-    while [ "$answer" != "y" -a "$answer" != "n" ]
-    do
-        echo "Please answer with y or n."
-        read -n 1 answer
-    done
-    if [ "$answer" == "y" ]
-    then
-        mv ~/.Xresources ~/.Xresources.bak
-    else
-        rm -f ~/.Xresources
-    fi
-fi
+localrc .vimrc
+localrc .bashrc
+localrc .zshrc
 
-if [ -d ~/.config/powerline ]
-then
-    echo "An existing .config/powerline folder has been found, do you wish keep a backup(.config/powerline.bak) of it? (y/n)"
-    read -n 1 answer
-    while [ "$answer" != "y" -a "$answer" != "n" ]
-    do
-        echo "Please answer with y or n."
-        read -n 1 answer
-    done
-    if [ "$answer" == "y" ]
-    then
-        mv ~/.config/powerline ~/.config/powerline.bak
-    else
-        rm -rf ~/.config/powerline
-    fi
-fi
+#if [ -d ~/.vim ]
+#then
+#    echo "An existing .vim folder has been found, do you wish to keep a backup(.vim.bak) of it? (y/n)"
+#    read -ns 1 answer
+#    while [ "$answer" != "y" -a "$answer" != "n" ]
+#    do
+#        echo "Please answer with y or n."
+#        read -ns 1 answer
+#    done
+#    if [ "$answer" == "y" ]
+#    then
+#        mv ~/.vim ~/.vim.bak
+#    else
+#        rm -rf ~/.vim
+#    fi
+#fi
+#
+#if [ -d ~/.zsh ]
+#then
+#    echo "An existing .zsh folder has been found, do you wish to keep a backup(.zsh.bak) of it? (y/n)"
+#    read -ns 1 answer
+#    while [ "$answer" != "y" -a "$answer" != "n" ]
+#    do
+#        echo "Please answer with y or n."
+#        read -ns 1 answer
+#    done
+#    if [ "$answer" == "y" ]
+#    then
+#        mv ~/.zsh ~/.zsh.bak
+#    else
+#        rm -rf ~/.zsh
+#    fi
+#fi
+#
+#if [ -f ~/.Xresources ]
+#then
+#    echo "An existing .Xresources file has been found, do you wish to keep a backup(.Xresources.bak) of it? (y/n)"
+#    read -ns 1 answer
+#    while [ "$answer" != "y" -a "$answer" != "n" ]
+#    do
+#        echo "Please answer with y or n."
+#        read -ns 1 answer
+#    done
+#    if [ "$answer" == "y" ]
+#    then
+#        mv ~/.Xresources ~/.Xresources.bak
+#    else
+#        rm -f ~/.Xresources
+#    fi
+#fi
+#
+#if [ -d ~/.config/powerline ]
+#then
+#    echo "An existing .config/powerline folder has been found, do you wish to keep a backup(.config/powerline.bak) of it? (y/n)"
+#    read -ns 1 answer
+#    while [ "$answer" != "y" -a "$answer" != "n" ]
+#    do
+#        echo "Please answer with y or n."
+#        read -ns 1 answer
+#    done
+#    if [ "$answer" == "y" ]
+#    then
+#        mv ~/.config/powerline ~/.config/powerline.bak
+#    else
+#        rm -rf ~/.config/powerline
+#    fi
+#fi
 
-if [ -f ~/.vimrc ]
-then
-    if [ ! -h ~/.vimrc ]
-    then
-        mv ~/.vimrc  ~/.vimrc.local
-        echo "Your previous .vimrc is now available under ~/.vimrc.local and can still be used to overwrite settings."
-    else
-        rm ~/.vimrc
-        touch ~/.vimrc.local
-    fi
-fi
+#if [ -f ~/.vimrc ]
+#then
+#    if [ ! -h ~/.vimrc ]
+#    then
+#        mv ~/.vimrc  ~/.vimrc.local
+#        echo "Your previous .vimrc is now available under ~/.vimrc.local and can still be used to overwrite settings."
+#    else
+#        rm ~/.vimrc
+#        touch ~/.vimrc.local
+#    fi
+#fi
+#
+#if [ -f ~/.zshrc ]
+#then
+#    mv ~/.zshrc ~/.zshrc.local
+#    echo "Your previous .zshrc is now available under ~/.zshrc.local and can still be used to overwrite settings."
+#else
+#    touch ~/.zshrc.local
+#fi
+#
+#if [ -f ~/.bashrc ]
+#then
+#    mv ~/.bashrc ~/.bashrc.local
+#    echo "Your previous .bashrc is now available under ~/.bashrc.local and can still be used to overwrite settings."
+#else
+#    touch ~/.bashrc.local
+#fi
 
-if [ -f ~/.zshrc ]
-then
-    mv ~/.zshrc ~/.zshrc.local
-    echo "Your previous .zshrc is now available under ~/.zshrc.local and can still be used to overwrite settings."
-else
-    touch ~/.zshrc.local
-fi
-
-if [ -f ~/.bashrc ]
-then
-    mv ~/.bashrc ~/.bashrc.local
-    echo "Your previous .bashrc is now available under ~/.bashrc.local and can still be used to overwrite settings."
-else
-    touch ~/.bashrc.local
-fi
+echo "Creating symlinks..."
 
 ln -s $DIR/.vimrc ~/.vimrc
 ln -s $DIR/.vim ~/.vim
