81b97cb123220a0bd6226c467ca759555924b07f
Evert Van Petegem <evert.van.petegem@gmail.com>
2014-06-08 23:42:11 +0200

    zsh: Use current command for searching through history

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,6 +1,28 @@
 setopt appendhistory autocd extendedglob nomatch
+setopt HIST_IGNORE_DUPS
 unsetopt beep notify
 bindkey -e
+
+typeset -A key
+
+key[Home]=${terminfo[khome]}
+key[End]=${terminfo[kend]}
+key[Insert]=${terminfo[kich1]}
+key[Delete]=${terminfo[kdch1]}
+key[Up]=${terminfo[kcuu1]}
+key[Down]=${terminfo[kcud1]}
+key[Left]=${terminfo[kcub1]}
+key[Right]=${terminfo[kcuf1]}
+key[PageUp]=${terminfo[kpp]}
+key[PageDown]=${terminfo[knp]}
+
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+
+bindkey "${key[Up]}" up-line-or-beginning-search
+bindkey "${key[Down]}" down-line-or-beginning-search
 
 autoload -U promptinit
 promptinit
