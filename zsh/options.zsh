dbffb8a237e0f2e10a357e0b8201b25aaad3d34b
Evert Van Petegem <evert.van.petegem@gmail.com>
2014-06-08 14:54:26 +0200

    zsh: Change terminal title

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -4,3 +4,20 @@ bindkey -e
 
 autoload -U promptinit
 promptinit
+
+case $TERM in
+  (*xterm* | *rxvt*)
+
+    # Write some info to terminal title.
+    # This is seen when the shell prompts for input.
+    function precmd {
+      print -Pn "\e]0;$TERM - $PWD\a"
+    }
+    # Write command and args to terminal title.
+    # This is seen while the shell waits for a command to complete.
+    function preexec {
+      printf "\e]0;%s\a" "$1"
+    }
+
+  ;;
+esac
